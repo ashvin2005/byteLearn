f8ac4d4372e9084975e17c8b5afa1ae5d92f2b8c
Ashvin Tiwari <ashvintiwari161@gmail.com>
2026-01-19 11:06:32 +0530

    ui: add bytelearning component

---
+++ frontend/src/components/ui/bytelearning.tsx
@@ -0,0 +1,44 @@
+export default function ByteLearning(){
+    return(
+
+        <div className="py-8 text-center ">
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.05s_infinite] mx-1">
+          b
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.1s_infinite] mx-1">
+          y
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.15s_infinite] mx-1">
+          t
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.2s_infinite] mx-1">
+          e
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.25s_infinite] mx-1">
+          l
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.3s_infinite] mx-1">
+          e
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.35s_infinite] mx-1">
+          a
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.4s_infinite] mx-1">
+          r
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.45s_infinite] mx-1">
+          n
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.5s_infinite] mx-1">
+          i
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.55s_infinite] mx-1">
+          n
+        </span>
+        <span className="inline-block animate-[bounce_0.3s_ease-in-out_0.6s_infinite] mx-1">
+          g
+        </span>
+      </div>
+        
+    )
+}
